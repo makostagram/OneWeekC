57e03bc16da7467d14a312481dab5cb413f6dca0
MakotoSENGOKU <biz_sengoku@outlook.com>
2022-11-19 14:43:20 +0900

    一週間で身につくC言語 day0-1
    実質、1日目の作業ファイルです。
    開業のエスケープシーケンスを連続で入力しても、1回しか反映されないのがなぜかはこれから探求します。

---
+++ day00/Helloworld.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main(){
+    printf("HelloWorld, just a joke!.\n");
+}
---
+++ day00/main2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+    printf("こんにちは。私の名前は%sです。\n年齢は%d歳です。\n\n","仙石 誠",56);
+    //printf("イニシャルは%cです。\n\n",'MS');
+    printf("イニシャルは、%cです。\n\n",'M');
+    printf("フルネームのイニシャルは%sです。\n\n","MS");
+    printf("%f + %f = %f\n\n",1.2,2.7,1.2 + 2.7);
+}
